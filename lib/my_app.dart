f9e4b5e2acdb7d33cb27bc323235cad09e08f6b9
Gleiton Fernandes <gtofernandes@hotmail.com>
2025-06-13 20:03:42 -0300

    Revert "Páginas de Aviso, Pontos de Venda e Pontos de Estacionamento"
    This reverts commit 51d759774ad9b667e02212f3d83128413cb9b680.

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,3 @@
-import 'package:estacionamentoSaoRoque/pages/avisos_page.dart';
-import 'package:estacionamentoSaoRoque/pages/pontos_estacionamento.dart';
-import 'package:estacionamentoSaoRoque/pages/pontos_venda.dart';
 import 'package:flutter/material.dart';
 import 'package:estacionamentoSaoRoque/pages/my_home_page.dart';
 
@@ -10,12 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Estacionamento São Roque',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const AvisosPage(),
+      home: const MyHomePage(title: 'Estacionamento'),
     );
   }
 }
